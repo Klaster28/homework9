e43beb2b891c4147696d16d858c0c8b8eb2daad5
Klaster28 <89096914521@mail.ru>
2022-10-17 20:29:25 +0300

    Коммит после третьего задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+
+    }
+
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1 () {
+
+        int sum = 0;
+        int [] arr = generateRandomArray();
+        for (int element:arr) {
+            sum += element;
+        }
+        System.out.println ("Сумма за месяц равна - " + sum);
+    }
+    public static void task2 () {
+        int[] arr = generateRandomArray();
+        Arrays.sort(arr);
+        System.out.println("Минимальная выплата за месяц составила - " + arr[0]);
+        System.out.println("Максимальная выплата за месяц составила - " + arr[29] );
+
+    }
+    public static void task3 () {
+        int sum = 0;
+        float averageValue = 0;
+        int [] arr = generateRandomArray();
+        for (int element:arr) {
+            sum += element;
+            averageValue = sum / 30;
+        }
+        System.out.println("Среднее значение выплат в течении месяца составляет - " + averageValue );
+
+    }
+    public static void task4 () {
 
     }
 }
